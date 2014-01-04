71fa0c89c335c35af239ad594b82547c8103f8ae
Ken Ohata <ohata@daraf.co.jp>
2014-01-04 17:21:25 +0900

    define application#index and root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SeboneJan2014::Application.routes.draw do
+  root 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
